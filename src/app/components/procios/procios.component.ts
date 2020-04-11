ef9a31f07e40ff88537b159c7d1f94377092806c
Luis C Ruiz <luko.luis12@gmail.com>
2020-04-11 15:44:26 -0500

    Todos los cambios

---
+++ src/app/components/procios/procios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-procios',
+  templateUrl: './procios.component.html',
+  styles: []
+})
+export class ProciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
